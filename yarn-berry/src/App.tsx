eb0f652e4b1aaaabb6254652a371e0fe337d5537
seongminn <berry9901@gmail.com>
2023-03-25 23:14:47 +0900

    yarn berry practive

---
+++ yarn-berry/src/App.tsx
@@ -0,0 +1,12 @@
+import New from './New';
+
+function App() {
+  return (
+    <div>
+      안녕하세요
+      <New msg='hi' />
+    </div>
+  );
+}
+
+export default App;
---
+++ yarn-berry/src/New.tsx
@@ -0,0 +1,5 @@
+const New = ({ msg }: { msg: 'hi' | 'bye' }) => {
+  return <div>{msg}</div>;
+};
+
+export default New;
